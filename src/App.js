e06af67f6f401199112d216b076f39a8fbc38433
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2021-06-24 16:52:19 -0400

    Basic google Maps window functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import React from "react";
+import MapContainer from "./MapContainer";
+
+const location = {
+  address: "82 Prospect St, Brooklyn, NY 11201",
+  lat: 40.70033337582707, 
+  lng:-73.98757818669164
+};
+
 
 function App() {
   return (
     <div className="App">
-      Let's find some beer.
+    <div style={{height: "50px"}}></div>
+      <MapContainer initialCenter={location}></MapContainer>
     </div>
   );
 }
---
+++ src/MapContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <Map 
+      google={this.props.google} 
+      zoom={14}
+      initialCenter={{
+        lat:this.props.initialCenter.lat,
+        lng:this.props.initialCenter.lng,
+      }}><Marker
+      title="Randolph's Beer"
+      position={{ 
+        lat:this.props.initialCenter.lat,
+        lng:this.props.initialCenter.lng
+      }}></Marker></Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper(
+  {apiKey: ''} // TODO - Setup .env variables
+)(MapContainer);
